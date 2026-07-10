78e95f5bcd3273fb3d63bceb1419835f73b2ec9e
Netzchhh <netzchhh@mail.ru>
2026-07-10 12:47:09 +0300

    Update Contest_5.cpp

--- Contest_5.cpp
+++ Contest_5.cpp
@@ -1,1 +1,31 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    int n;
+    if (!(cin >> n)) return 0;
+
+    vector<vector<int>> a(n, vector<int>(n));
+    for (int i = 0; i < n; ++i) {
+        for (int j = 0; j <= i; ++j) {
+            cin >> a[i][j];
+        }
+    }
+
+    for (int i = n - 2; i >= 0; --i) {
+        for (int j = 0; j <= i; ++j) {
+            a[i][j] += max(a[i + 1][j], a[i + 1][j + 1]);
+        }
+    }
+
+    cout << a[0][0] << "\n";
+
+    return 0;
+}
 
