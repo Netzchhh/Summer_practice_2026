ac8e340890866f84a6d4e41f2089e4150eb33cc8
Netzchhh <netzchhh@mail.ru>
2026-07-10 12:48:11 +0300

    Update Contest_7.cpp

--- Contest_7.cpp
+++ Contest_7.cpp
@@ -1,1 +1,31 @@
+#include <iostream>
+#include <algorithm>
+
+using namespace std;
+
+void solve() {
+    long long n, a, b;
+    cin >> n >> a >> b;
+    
+    if (2 * a <= b) {
+        cout << n * a << "\n";
+    } else {
+        long long pairs = n / 2;
+        long long remainder = n % 2;
+        cout << pairs * b + remainder * a << "\n";
+    }
+}
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    
+    int t;
+    cin >> t;
+    while (t--) {
+        solve();
+    }
+    
+    return 0;
+}
 
